7786807b4952233f6ba36b2279a61bbc311cabe3
Jessica Eze <ezejessica@gmail.com>
2018-07-30 11:58:58 +0100

    KB's Updates...

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 /**
   * Main JS File
   *
@@ -20,59 +20,74 @@ let sfc = {
     images:     document.querySelectorAll('.content-area p img'),
     iframes:    document.querySelectorAll('iframe'),
     figure:     document.createElement('figure')
-}
+};
 
 
 // Output current year
-sfc.insertCurrentYear =  function() {
-    return this.elem.innerText = this.fullYear
-}
+sfc.insertCurrentYear = function () {
+    return this.elem.innerText = this.fullYear;
+};
 
 
 // Affix mainbar to top on scroll to it
 sfc.affixMainbar = function() {
-    let that = this
+    let that = this;
     window.addEventListener('scroll', function() {
-        let scrollY = window.scrollY
+        let scrollY = window.scrollY;
         if (scrollY > (that.topOffset + that.topHeight)) {
-            that.mainbar.classList.add('is-fixed')
+            that.mainbar.classList.add('is-fixed');
         } else {
-            that.mainbar.classList.remove('is-fixed')
+            that.mainbar.classList.remove('is-fixed');
         }
-    })
-}
+    });
+};
 
 
 // Replace img and ifram etc with html5 types...
-sfc.replaceMedia = function() {
-    let that = this
+sfc.replaceMedia = function () {
+    let that = this;
     // Replacing all images with figure and caption...
     if (that.images) {
-        that.images.forEach(el => {  
-            let parentImg   = el.closest('p'),
-                figure      = document.createElement('figure')    
+        that.images.forEach(el => {
+            let parentImg = el.closest('p'),
+                figure = document.createElement('figure');
             figure.innerHTML = `
                 <img src="${el.src}" alt="${el.alt}">
                 <figcaption>${el.alt}</figcaption>
-            `
-            parentImg.parentNode.replaceChild(figure, parentImg)
-        })
+            `;
+            parentImg.parentNode.replaceChild(figure, parentImg);
+        });
     }
     
     // Replacing all iframes with figure and figcaption...
     if (that.iframes) {
         that.iframes.forEach(el => {
-            that.figure.classList.add('video-wrapper')    
+            that.figure.classList.add('video-wrapper');
             that.figure.innerHTML = `
                 <iframe src="${el.src}" frameborder="0" allowfullscreen></iframe>
-            `
-            el.parentNode.replaceChild(that.figure, el)
-        })
+            `;
+            el.parentNode.replaceChild(that.figure, el);
+        });
     }
-}
+};
+
+sfc.toggleNav = function () {
+    let navEl = document.querySelector('[toggle-nav]'),
+        toggler = document.querySelector('[nav-toggler]');
+    document.addEventListener('click', function (e) {
+        if (e.target.matches('[nav-toggler]')) {
+            navEl.classList.contains('is-active') ? navEl.classList.remove('is-active') : navEl.classList.add('is-active');
+            e.target.classList.contains('is-clicked') ? e.target.classList.remove('is-clicked') : e.target.classList.add('is-clicked');
+        } else {
+            navEl.classList.remove('is-active');
+            toggler.classList.remove('is-clicked');
+        }
+    }, false);
+};
 
 
 // Calls
-sfc.affixMainbar()
-sfc.insertCurrentYear()
-sfc.replaceMedia()
+sfc.toggleNav();
+sfc.affixMainbar();
+sfc.insertCurrentYear();
+sfc.replaceMedia();
